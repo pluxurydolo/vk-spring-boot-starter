762a1454a0901f5da965b1e52817189b77edec07
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-17 22:05:31 +0300

    17.04.2026

--- src/main/java/com/pluxurydolo/vk/config/VkAutoConfiguration.java
+++ src/main/java/com/pluxurydolo/vk/config/VkAutoConfiguration.java
@@ -2,12 +2,10 @@ package com.pluxurydolo.vk.config;
 
 import com.pluxurydolo.vk.properties.Delay;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
 @AutoConfiguration
-@ConditionalOnProperty(prefix = "vk", name = "enabled", havingValue = "true")
 @EnableConfigurationProperties(Delay.class)
 @Import({
     VkClientConfiguration.class,
