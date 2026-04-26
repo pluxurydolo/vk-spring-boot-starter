5ac17c5e5feda4f5b48051e72faeb6293a193f1c
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-26 21:10:27 +0300

    26.04.2026

--- src/main/java/com/pluxurydolo/vk/client/VkImageClient.java
+++ src/main/java/com/pluxurydolo/vk/client/VkImageClient.java
@@ -1,52 +1,29 @@
 package com.pluxurydolo.vk.client;
 
 import com.pluxurydolo.vk.dto.PostImageRequest;
-import com.pluxurydolo.vk.step.image.PhotoUploader;
-import com.pluxurydolo.vk.step.image.WallPhotoSaver;
-import com.pluxurydolo.vk.step.image.WallPoster;
-import com.pluxurydolo.vk.step.image.WallUploadServerRetriever;
-import com.pluxurydolo.vk.util.FileUtils;
-import com.vk.api.sdk.client.actors.GroupActor;
-import com.vk.api.sdk.client.actors.UserActor;
-import com.vk.api.sdk.objects.photos.responses.GetWallUploadServerResponse;
+import com.pluxurydolo.vk.exception.VkImageUploadException;
+import com.pluxurydolo.vk.step.image.VkImageSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
-
-import java.io.File;
+import reactor.core.scheduler.Schedulers;
 
 public class VkImageClient {
-    private final WallUploadServerRetriever wallUploadServerRetriever;
-    private final PhotoUploader photoUploader;
-    private final WallPhotoSaver wallPhotoSaver;
-    private final WallPoster wallPoster;
-    private final FileUtils fileUtils;
+    private static final Logger LOGGER = LoggerFactory.getLogger(VkImageClient.class);
 
-    public VkImageClient(
-        WallUploadServerRetriever wallUploadServerRetriever,
-        PhotoUploader photoUploader,
-        WallPhotoSaver wallPhotoSaver,
-        WallPoster wallPoster,
-        FileUtils fileUtils
-    ) {
-        this.wallUploadServerRetriever = wallUploadServerRetriever;
-        this.photoUploader = photoUploader;
-        this.wallPhotoSaver = wallPhotoSaver;
-        this.wallPoster = wallPoster;
-        this.fileUtils = fileUtils;
+    private final VkImageSender vkImageSender;
+
+    public VkImageClient(VkImageSender vkImageSender) {
+        this.vkImageSender = vkImageSender;
     }
 
-    public Mono<String> postImageToGroup(PostImageRequest postImageRequest) {
-        byte[] image = postImageRequest.image();
-        String caption = postImageRequest.caption();
-        UserActor userActor = postImageRequest.userActor();
-        GroupActor groupActor = postImageRequest.groupActor();
-
-        Mono<GetWallUploadServerResponse> wallUploadServer = wallUploadServerRetriever.retrieve(userActor, groupActor);
-        Mono<File> file = fileUtils.createTempFile("file", ".jpg", image);
-
-        return Mono.zip(wallUploadServer, file)
-            .flatMap(zip -> photoUploader.upload(zip.getT1(), zip.getT2()))
-            .flatMap(response -> wallPhotoSaver.save(response, userActor, groupActor))
-            .flatMap(response -> wallPoster.post(response, userActor, groupActor, caption))
-            .thenReturn(caption);
+    public Mono<String> sendImageToGroup(PostImageRequest request) {
+        return vkImageSender.sendToGroup(request)
+            .doOnSuccess(_ -> LOGGER.info("cepb [vk-starter] Изображение успешно опубликовано"))
+            .onErrorResume(throwable -> {
+                LOGGER.info("abka [vk-starter] Произошла ошибка при публикации изображения");
+                return Mono.error(new VkImageUploadException(throwable));
+            })
+            .subscribeOn(Schedulers.boundedElastic());
     }
 }
--- src/test/java/com/pluxurydolo/vk/client/VkImageClientTests.java
+++ src/test/java/com/pluxurydolo/vk/client/VkImageClientTests.java
@@ -1,17 +1,9 @@
 package com.pluxurydolo.vk.client;
 
 import com.pluxurydolo.vk.dto.PostImageRequest;
-import com.pluxurydolo.vk.step.image.PhotoUploader;
-import com.pluxurydolo.vk.step.image.WallPhotoSaver;
-import com.pluxurydolo.vk.step.image.WallPoster;
-import com.pluxurydolo.vk.step.image.WallUploadServerRetriever;
-import com.pluxurydolo.vk.util.FileUtils;
+import com.pluxurydolo.vk.step.image.VkImageSender;
 import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.client.actors.UserActor;
-import com.vk.api.sdk.objects.photos.responses.GetWallUploadServerResponse;
-import com.vk.api.sdk.objects.photos.responses.PhotoUploadResponse;
-import com.vk.api.sdk.objects.photos.responses.SaveWallPhotoResponse;
-import com.vk.api.sdk.objects.wall.responses.PostResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,10 +11,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 
-import java.io.File;
-
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static reactor.test.StepVerifier.create;
@@ -31,56 +20,33 @@ import static reactor.test.StepVerifier.create;
 class VkImageClientTests {
 
     @Mock
-    private WallUploadServerRetriever wallUploadServerRetriever;
-
-    @Mock
-    private PhotoUploader photoUploader;
-
-    @Mock
-    private WallPhotoSaver wallPhotoSaver;
-
-    @Mock
-    private WallPoster wallPoster;
-
-    @Mock
-    private FileUtils fileUtils;
-
-    @Mock
-    private GetWallUploadServerResponse getWallUploadServerResponse;
-
-    @Mock
-    private File file;
-
-    @Mock
-    private PhotoUploadResponse photoUploadResponse;
-
-    @Mock
-    private SaveWallPhotoResponse saveWallPhotoResponse;
-
-    @Mock
-    private PostResponse postResponse;
+    private VkImageSender vkImageSender;
 
     @InjectMocks
     private VkImageClient vkImageClient;
 
     @Test
-    void testPostImageToGroup() {
-        when(wallUploadServerRetriever.retrieve(any(), any()))
-            .thenReturn(Mono.just(getWallUploadServerResponse));
-        when(fileUtils.createTempFile(anyString(), anyString(), any()))
-            .thenReturn(Mono.just(file));
-        when(photoUploader.upload(any(), any()))
-            .thenReturn(Mono.just(photoUploadResponse));
-        when(wallPhotoSaver.save(any(), any(), any()))
-            .thenReturn(Mono.just(saveWallPhotoResponse));
-        when(wallPoster.post(any(), any(), any(), anyString()))
-            .thenReturn(Mono.just(postResponse));
+    void testSendImageToGroup() {
+        when(vkImageSender.sendToGroup(any()))
+            .thenReturn(Mono.just(""));
 
-        Mono<String> result = vkImageClient.postImageToGroup(postImageRequest());
+        Mono<String> result = vkImageClient.sendImageToGroup(postImageRequest());
 
         create(result)
-            .expectNext("caption")
+            .expectNext("")
             .verifyComplete();
+    }
+
+    @Test
+    void testSendImageToGroupWhenExceptionOccurred() {
+        when(vkImageSender.sendToGroup(any()))
+            .thenReturn(Mono.error(new RuntimeException()));
+
+        Mono<String> result = vkImageClient.sendImageToGroup(postImageRequest());
+
+        create(result)
+            .expectError(RuntimeException.class)
+            .verify();
     }
 
     private static PostImageRequest postImageRequest() {
